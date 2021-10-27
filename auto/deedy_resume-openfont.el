9f418888d0e16009d3930a48131d9e64e7c58815
Joshua Barrett <jjbarr.public@gmail.com>
2021-10-27 09:22:24 -0400

    committed resume

---
+++ auto/deedy_resume-openfont.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook
+ "deedy_resume-openfont"
+ (lambda ()
+   (TeX-run-style-hooks
+    "latex2e"
+    "deedy-resume-openfont"
+    "deedy-resume-openfont10"
+    "fancyhdr"
+    "article"
+    "art10"))
+ :latex)
+
